b683b12fd3ae6e6ead1f6547ed08e83b69e2d639
Oumar Mersile Atti Mahamat <74494155+mersile-atti@users.noreply.github.com>
2023-12-19 21:17:00 +0100

    Update API fixed

--- models/bloodRequestModel.js
+++ models/bloodRequestModel.js
@@ -11,7 +11,7 @@ const bloodDonationRequestSchema = new mongoose.Schema({
     hospitalLocation: { type: String, required: true },
     status: { type: String, enum: ['pending', 'approved', 'rejected', 'completed'], default: 'pending' }, // You can have different statuses like 'Open', 'Fulfilled', 'Closed', etc.
     responders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Reference to users responding to the request
-    createdAt: { type: Date, default: Date.now },
+    createdAt: String,
     urgency: { type: String, enum: ['Accident/Emergency', 'Chemotherapy', 'Maternity', 'Surgery'], required: [true, 'Urgency is required'],},
     pintsNeeded: { type: Number, required: true },
     diagnosis: String,
