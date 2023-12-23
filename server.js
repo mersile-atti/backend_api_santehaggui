be2d470807a4a1a3784fd3d2b5beb9b8c242485d
Oumar Mersile Atti Mahamat <74494155+mersile-atti@users.noreply.github.com>
2023-12-23 03:48:26 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -13,7 +13,33 @@ dotenv.config();
 
 
 
-
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+  } from "@aws-sdk/client-secrets-manager";
+  
+  const secret_name = "prod/backend/santehaggui";
+  
+  const client = new SecretsManagerClient({
+    region: "eu-north-1",
+  });
+  
+  let response;
+  
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+  
+  const secret = response.SecretString;
 
 
 // Load the service account key file
