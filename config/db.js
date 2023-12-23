77a1681f06911911e79a85190c0fdd61d7810098
Oumar Mersile Atti Mahamat <74494155+mersile-atti@users.noreply.github.com>
2023-12-23 04:29:43 +0100

    Prepare for deployment

--- config/db.js
+++ config/db.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+const AWS = require('aws-sdk');
+const secretsManager = new AWS.SecretsManager({ region: 'eu-north-1' });
+
+const secretName = 'prod/backend/mongodb';
+
+secretsManager.getSecretValue({
+  SecretId: secretName
+}).then(data => {
+  const mongoUri = JSON.parse(data.SecretString).value;
+  process.env.MONGO_URI = mongoUri;
+
+  const mongoose = require('mongoose');
+
+  connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDb Connect: ${conn.connection.host}, ${conn.connection.name}`);
+      const conn = await mongoose.connect(process.env.MONGO_URI)
+      console.log(`MongoDb Connect: ${conn.connection.host}, ${conn.connection.name}`);
     } catch (error) {
-        console.log(error);
-        process.exit(1);    
+      console.error(error);
+      process.exit(1);
     }
-};
+  };
+
+  module.exports = connectDB;
+}).catch(error => {
+  console.error(error);
+});
+
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -13,41 +13,6 @@ dotenv.config();
 
 
 
-const {
-    SecretsManagerClient,
-    GetSecretValueCommand,
-  } = require('@aws-sdk/client-secrets-manager');
-  
-  const secret_name = "prod/backend/santehaggui";
-  
-  const client = new SecretsManagerClient({ region: "eu-north-1" });
-  
-  const getSecretValuePromise = () => {
-    return new Promise((resolve, reject) => {
-      client.send(
-        new GetSecretValueCommand({
-          SecretId: secret_name,
-          VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-        }),
-        (error, response) => {
-          if (error) {
-            reject(error);
-          } else {
-            const secretData = JSON.parse(response.SecretString);
-            resolve({ secret: secretData.value });
-          }
-        }
-      );
-    });
-  };
-  
-  getSecretValuePromise()
-    .then((secretData) => {
-      console.log('Secret value:', secretData.secret);
-    })
-    .catch((error) => {
-      console.error('Error retrieving secret:', error);
-    });
 
 
 // Load the service account key file
