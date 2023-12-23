347f968517ef44c555f639f29d90db68290b6c34
Oumar Mersile Atti Mahamat <74494155+mersile-atti@users.noreply.github.com>
2023-12-23 23:02:47 +0100

    Prepare for deployment

--- controllers/healthRecordsControllers.js
+++ controllers/healthRecordsControllers.js
@@ -312,7 +312,19 @@ const updateUserPic = asyncHandler(async (req, res) => {
 //@route DELETE /api/healthRecords/profile/pic
 //@access Private
 
+const deleteUserPic = asyncHandler(async (req, res) => {
+    const userID = req.user.id;
 
+    const userPic = await UserProfilePic.findOne({ user: userID });
+    if (!userPic) {
+        throw new Error('User profile picture not found');
+    }
+
+    await userPic.remove();
+
+    res.status(200).json({ message: 'User profile picture deleted successfully' });
+
+})
 
 module.exports=  {
     getAllEmergencyProfiles,
@@ -323,5 +335,6 @@ module.exports=  {
     setProfilePic,
     getUserPic,
     updateUserPic,
+    deleteUserPic
     
 }
