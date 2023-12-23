8bfa8ec4a41bc72d351a787c88bdca5bd9c84107
Oumar Mersile Atti Mahamat <74494155+mersile-atti@users.noreply.github.com>
2023-12-23 04:11:12 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -13,33 +13,41 @@ dotenv.config();
 
 
 
-import {
+const {
     SecretsManagerClient,
     GetSecretValueCommand,
-  } from "@aws-sdk/client-secrets-manager";
+  } = require('@aws-sdk/client-secrets-manager');
   
   const secret_name = "prod/backend/santehaggui";
   
-  const client = new SecretsManagerClient({
-    region: "eu-north-1",
-  });
+  const client = new SecretsManagerClient({ region: "eu-north-1" });
   
-  let response;
+  const getSecretValuePromise = () => {
+    return new Promise((resolve, reject) => {
+      client.send(
+        new GetSecretValueCommand({
+          SecretId: secret_name,
+          VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        }),
+        (error, response) => {
+          if (error) {
+            reject(error);
+          } else {
+            const secretData = JSON.parse(response.SecretString);
+            resolve({ secret: secretData.value });
+          }
+        }
+      );
+    });
+  };
   
-  try {
-    response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secret_name,
-        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-      })
-    );
-  } catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw error;
-  }
-  
-  const secret = response.SecretString;
+  getSecretValuePromise()
+    .then((secretData) => {
+      console.log('Secret value:', secretData.secret);
+    })
+    .catch((error) => {
+      console.error('Error retrieving secret:', error);
+    });
 
 
 // Load the service account key file
